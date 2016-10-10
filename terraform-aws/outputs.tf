c99979bce44bc84cd30cf9c65fcb65108f2dbbb3
Said Sef <saidsef@gmail.com>
2016-10-10 01:19:00 +0100

    reduce terraform output resource

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -1,24 +1,4 @@
 
-output "public_subnets_id" {
-  value = "${module.vpc_subnets.public_subnets_id}"
-}
-
-output "private_subnets_id" {
-  value = "${module.vpc_subnets.private_subnets_id}"
-}
-
-output "ssh_sg_id" {
-  value = "${module.ssh_sg.ssh_sg_id}"
-}
-
-output "app_sg_id" {
-  value = "${module.app_sg.app_sg_id}"
-}
-
-output "elb_sg_id" {
-  value = "${module.elb_sg.elb_sg_id}"
-}
-
 output "elb_dns_name" {
   value = "${module.elb.elb_dns_name}"
 }
